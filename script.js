2cc668775e370fd2c25240e5e04d75bc10fe612d
Idris Mohammed <nasiru.idris4646@gmail.com>
2024-09-17 06:45:20 +0100

    work

--- script.js
+++ script.js
@@ -25,23 +25,23 @@ top1.innerHTML = `Welcome ${username}! What meal are you getting from us?`;
 
 btn.addEventListener('click', function() {
     let userMainMeal = q1.value.toLowerCase(); 
-    let userProtien = q2.value.toLowerCase();
+    // let userProtien = q2.value.toLowerCase();
     let userDrink = q3.value.toLowerCase();
 
     let a = document.getElementById('question1').value
     let b = document.getElementById('question3').value
 
-    let c = document.getElementById('question2').value
+    // let c = document.getElementById('question2').value
 
 
     let aErr = document.getElementById('aErr')
     let bErr = document.getElementById('bErr')
-    let cErr = document.getElementById('cErr')
+    // let cErr = document.getElementById('cErr')
 
 
 
     let avalaibleMeal = mainMeal.includes(userMainMeal);
-    let avalaibleProtien = protien.includes(userProtien);
+    // let avalaibleProtien = protien.includes(userProtien);
     let avalaibleDrink = drinks.includes(userDrink);
 
 
@@ -55,16 +55,7 @@ btn.addEventListener('click', function() {
         aErr.innerHTML = ""
     }
     if(b === ""){
-        cErr.innerHTML =  ' Drinks feild cant be blank cant be left blank'
-        valid = false
-        console.log(valid)
-
-
-    }else{
-        cErr.innerHTML = ""
-    }
-    if(c === ""){
-        bErr.innerHTML =  'feild cant be blank cant be left blank'
+        bErr.innerHTML =  ' Drinks feild cant be blank cant be left blank'
         valid = false
         console.log(valid)
 
@@ -72,50 +63,63 @@ btn.addEventListener('click', function() {
     }else{
         bErr.innerHTML = ""
     }
-    if (avalaibleMeal && avalaibleDrink && avalaibleProtien) {
-        alert (`Thank you ${username}, for ordering ${userMainMeal}, ${userProtien}, and ${userDrink}! Please proceed to payment.`);
+    // if(c === ""){
+    //     bErr.innerHTML =  'feild cant be blank cant be left blank'
+    //     valid = false
+    //     console.log(valid)
+
+
+    // }else{
+    //     bErr.innerHTML = ""
+    // }
+    if (avalaibleMeal && avalaibleDrink) {
+        alert (`Thank you ${username}, for ordering ${userMainMeal} and ${userDrink}! Please proceed to payment.`);
         selectedMeal.innerHTML = '';
         // top1.innerHTML = `Thank you ${username}, for making this purchase with us!`;
         top1.innerHTML = '';
         q1.value = '';  
-        q2.value = '';  
+        // q2.value = '';  
 
         q3.value = '';  
     } else if (avalaibleMeal) {
-        selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userDrink} or ${userProtien}. Please choose a drink from our Drink menu: ${drinks.join(', ')} and a Protien From our Protien Menu: ${protien.join(', ')}.`;
+        selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userDrink} Please choose a drink from our Drink menu: ${drinks.join(', ')} and a Protien From our Protien Menu: ${protien.join(', ')}.`;
         top1.innerHTML = `Sorry ${username}, we dont have all that you've orded for`
-        q2.value = '';
+        // q2.value = '';
     
 
         q3.value = '';  
-    }else if (avalaibleProtien) {
-        selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userMainMeal} and ${userDrink}. Please choose a main meal from our menu: ${mainMeal.join(', ')} and a Drink from our Drinks Menu  ${drinks.join(', ')}.`;
-        top1.innerHTML = `Sorry ${username}, we dont have all what you've orded for`
-        q1.value = ''; 
-        q3.value = '';  
-    } 
-    else if (avalaibleDrink) {
-        selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userMainMeal} and ${userProtien}. Please choose a main meal from our menu: ${mainMeal.join(', ')} and a Protien from our Protien Menu: ${userProtien}`;
-        top1.innerHTML = `Sorry ${username}, we dont have all what you've orded for`
-        q1.value = ''; 
-        q2.value = '';  
-    }else if (avalaibleMeal && avalaibleProtien){
-        selectedMeal.innerHTML = ` sorry ${username} please fill in the form`
-    } 
-    else if (avalaibleMeal && avalaibleDrink){
-        selectedMeal.innerHTML = ` sorry ${username} please fill in the form`
-    } 
-    else if (avalaibleProtien && avalaibleDrink){
-        selectedMeal.innerHTML = ` sorry ${username} please fill in the form`
     }
+    // else if (avalaibleProtien) {
+    //     selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userMainMeal} and ${userDrink}. Please choose a main meal from our menu: ${mainMeal.join(', ')} and a Drink from our Drinks Menu  ${drinks.join(', ')}.`;
+    //     top1.innerHTML = `Sorry ${username}, we dont have all what you've orded for`
+    //     q1.value = ''; 
+    //     q3.value = '';  
+    // } 
+    else if (avalaibleDrink) {
+        selectedMeal.innerHTML = `Sorry ${username}! We don't have ${userMainMeal} . Please choose a main meal from our menu: ${mainMeal.join(', ')}`;
+        top1.innerHTML = `Sorry ${username}, we dont have all what you've orded for`
+        q1.value = ''; 
+        // q2.value = ''; 
+    } 
+    // else if (!avalaibleMeal && avalaibleProtien){
+    //     selectedMeal.innerHTML = ` sorry ${username}! we dont have ${userDrink}, Please Choose a Drink from our Drink  menu: ${drinks.join(', ')}`
+    //     q3.value = '';  
+
+    // } 
+    // // else if (avalaibleMeal && avalaibleDrink){
+    //     selectedMeal.innerHTML = ` sorry ${username} please fill in the form`
+    // } 
+    // else if (avalaibleProtien && avalaibleDrink){
+    //     selectedMeal.innerHTML = ` sorry ${username} please fill in the form`
+    // }
     else {
-        selectedMeal.innerHTML = `Sorry ${username}, We don't have ${userMainMeal}, ${userProtien} or ${userDrink}, Please choose from our menu: Main Meals - ${mainMeal.join(', ')}, Protien - ${protien.join(', ')} Drinks - ${drinks.join(', ')}.`;
+        selectedMeal.innerHTML = `Sorry ${username}, We don't have ${userMainMeal} or ${userDrink}, Please choose from our menu: Main Meals - ${mainMeal.join(', ')},  Drinks - ${drinks.join(', ')}.`;
 
         top1.innerHTML = `Sorry ${username}, we dont have what you've ordered for!`
 
        
         q1.value = ''; 
-        q2.value = '';  
+        // q2.value = '';  
 
         q3.value = '';  
 
